9dcaea81cba0dfde3ad7b1645b555fbedfdc4165
DANNIMENNA89 <danielmenajimenez89@gmail.com>
2024-04-29 05:01:47 +0200

    Commit para actualizar proyecto con nuestra version

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from '@modules/home/pages/home-page/home-page.component';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+ {
+  path:'',
+  // loadChildren:() => import(`./modules/home/home.module`).then(m => m.HomeModule)
+  component: HomePageComponent
+ }
+
+
+];
+
---
+++ src/app/modules/home/home.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SharedModule } from '../../shared/shared.module';
+import { HomeRoutingModule } from './home-routing.module';
+
+@NgModule({
+  declarations: [
+    HomePageComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    SharedModule
+  ]
+})
+export class HomeModule { }
+
