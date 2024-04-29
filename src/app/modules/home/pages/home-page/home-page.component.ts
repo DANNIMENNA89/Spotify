0bd480510eac1d63223923e000cb75c7149a2356
DANNIMENNA89 <danielmenajimenez89@gmail.com>
2024-04-29 06:11:09 +0200

    Antes de remover las rutas

--- src/app/modules/home/pages/home-page/home-page.component.ts
+++ src/app/modules/home/pages/home-page/home-page.component.ts
@@ -1,9 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+
 
 @Component({
   selector: 'app-home-page',
+  standalone:true,
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css'],
+  imports:[
+    SideBarComponent
+]
 })
 export class HomePageComponent {
 }
