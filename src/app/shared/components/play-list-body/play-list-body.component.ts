8f38af0a0f37c2e121f8131dee8ab6df8e371da6
DANNIMENNA89 <danielmenajimenez89@gmail.com>
2024-05-02 20:24:01 +0200

    Pipe y directives

--- src/app/shared/components/play-list-body/play-list-body.component.ts
+++ src/app/shared/components/play-list-body/play-list-body.component.ts
@@ -1,12 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import * as dataRaw from "../../../data/tracks.json"
+import { TrackModel } from '@core/models/tracks.model';
+import { OrderListPipe } from '@shared/pipe/order-list.pipe';
 
 @Component({
   selector: 'app-play-list-body',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    OrderListPipe
+  ],
   templateUrl: './play-list-body.component.html',
   styleUrl: './play-list-body.component.css'
 })
-export class PlayListBodyComponent {
+export class PlayListBodyComponent implements OnInit {
 
+  tracks: Array<TrackModel> = []
+  optionSort:{property:string | null, order:string} = {property: null, order: 'asc'}
+  constructor () {}
+
+  ngOnInit(): void {
+    const { data }: any = (dataRaw as any).default
+    this.tracks = data
+  }
+
+  changeSort(property: string): void{
+    const { order } = this.optionSort
+    this.optionSort = {
+      property,
+      order: order === 'asc' ? 'desc' : 'asc'
+    }
+    console.log(this.optionSort);
+
+  }
 }
---
+++ src/app/shared/directives/img-broken.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appImgBroken]',
+  standalone: true
+})
+export class ImgBrokenDirective {
+
+  constructor() { }
+
+}
