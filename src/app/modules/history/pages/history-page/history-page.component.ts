5e9ba98fcc0d4d7559831af0aa2a2910198d5a9f
DANNIMENNA89 <danielmenajimenez89@gmail.com>
2024-05-02 10:02:58 +0200

    Carpeta History

---
+++ src/app/modules/history/pages/history-page/history-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hostory-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './history-page.component.html',
+  styleUrl: './history-page.component.css'
+})
+export class HostoryPageComponent {
+
+}
