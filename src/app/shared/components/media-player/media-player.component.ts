a76e79753a915ab8eccf9162533d35a05fc1310a
DANNIMENNA89 <danielmenajimenez89@gmail.com>
2024-05-19 22:02:08 +0200

    Reproductor acabado

--- src/app/shared/components/media-player/media-player.component.ts
+++ src/app/shared/components/media-player/media-player.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, ElementRef, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { TrackModel } from '@core/models/tracks.model';
 import { MultimediaService } from '@shared/services/multimedia.service';
 import { response } from 'express';
@@ -14,16 +14,31 @@ import { Subscription } from 'rxjs';
 })
 export class MediaPlayerComponent implements OnInit, OnDestroy {
 
+    @ViewChild('progressBar') progressBar:ElementRef = new ElementRef('')
   listObservers$: Array<Subscription> = []
+  state: string = 'paused'
 
   constructor(public multimediaService: MultimediaService){ }
 
   ngOnInit(): void {
+    const observer1$ = this.multimediaService.playerStatus$
+    .subscribe(status => this.state =status)
 
+    this.listObservers$ = [observer1$]
   }
 
   ngOnDestroy(): void {
     this.listObservers$.forEach(u => u.unsubscribe())
+
+  }
+
+  handlePosition(event: MouseEvent): void{
+    const elNative: HTMLElement = this.progressBar.nativeElement
+    const { clientX } = event
+    const {x, width } = elNative.getBoundingClientRect()
+    const clicX = clientX - x
+    const percerntageFromX = (clicX * 100) / width
+    this.multimediaService.seekAudio(percerntageFromX)
 
   }
 
